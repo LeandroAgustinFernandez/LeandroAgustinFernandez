3b5f36e51b77b013cd835e1a21d7f4a52a8f7ff2
= <=>
2021-08-11 14:20:40 -0300

    Api-Rest-Mongo-DB 1.0.1

--- routers/usuarios.js
+++ routers/usuarios.js
@@ -1,25 +1,47 @@
 const express = require("express");
 const usuarioModel = require("../models/usuario.model");
 const ruta = express.Router();
+const Joi = require("joi");
 
-// ruta.get("/", async (req, res) => {
-//   res.json("Listo el GET de usuarios");
-// });
+const usuarioValidacion = Joi.object({
+  nombre: Joi.string().alphanum().min(3).max(30).required(),
+
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
+  email: Joi.string()
+    .email()
+    .pattern(
+      new RegExp(
+        "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$"
+      )
+    ),
+});
 
 ruta.post("/", (req, res) => {
   let body = req.body;
-  let resultado = crearUsuario(body);
-  resultado
-    .then((doc) => {
-      res.json({
-        valor: doc,
+  const { error, value } = usuarioValidacion.validate({
+    nombre: body.nombre,
+    password: body.password,
+    email: body.email,
+  });
+  if (!error) {
+    let resultado = crearUsuario(body);
+    resultado
+      .then((doc) => {
+        res.json({
+          valor: doc,
+        });
+      })
+      .catch((err) => {
+        res.status(400).json({
+          error: err,
+        });
       });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        error: err,
-      });
+  } else {
+    res.status(400).json({
+      error: error,
     });
+  }
 });
 
 ruta.put("/:id", (req, res) => {
